cb38be9e22fd740cc7439d3cb21c04bf0c4c1fe7
nehachougule1998 <nchougule734@gmail.com>
2022-07-24 18:23:35 +0530

    RestController

---
+++ AddressBookAppDevelopement/src/main/java/com/bridgelabz/addressbookappdevelopement/dto/AddressBookDTO.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.addressbookappdevelopement.dto;
+
+import jdk.jfr.DataAmount;
+import lombok.Data;
+
+@Data
+
+public class AddressBookDTO {
+    public String firstName;
+    public String lastName;
+    public String phoneNumber;
+    public String email;
+    public String address;
+    public String city;
+    public String state;
+    public int zip;
+
+    public AddressBookDTO(String firstName, String lastName, String phoneNumber, String email,
+                          String address, String city, String state, int zip) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+}
+
